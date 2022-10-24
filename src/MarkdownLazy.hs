a2c3cec58ac3955fd9c227f2aba43b0455026551
Oon Guo Liang <oon.guo.liang@gmail.com>
2022-10-24 18:29:59 +0800

    nonconcrete prisms

--- src/MarkdownLazy.hs
+++ src/MarkdownLazy.hs
@@ -3,52 +3,54 @@
 
 import MarkdownParse (withinMany)
 
-import qualified Data.Text as T
+import Data.Text as T
 
 import Text.Parsec
 import Control.Lens (Prism', prism, prism', withPrism)
 import Control.Applicative hiding (many)
 
-italic = Italic . T.pack <$> withinMany (char '*') (noneOf (['*']))
+type Parser a = Parsec Text () a
 
-data Italic = Italic T.Text deriving Show
-newtype Context = Context T.Text deriving Show
+italic :: Parser Italic
+italic = Italic . pack <$> withinMany (char '*') (noneOf (['*']))
 
+data Italic = Italic Text deriving Show
+newtype Context = Context Text deriving Show
 
-data Optic a b = Optic {
-    matchOptic :: a -> Context -> Maybe (b, Context)
-  , buildOptic :: Context -> b -> (a, Context)
-}
+type Optic a b = Prism' (a, Context) (b, Context)
 
-andThen :: Optic a x -> Optic T.Text y -> Optic a (x, y)
-andThen (Optic m b) (Optic m' b') = Optic match build
-  where
-    match a ctx = case m a ctx of
-      Just (x, (Context unconsumed)) -> case m' unconsumed (Context "") of
+withPrism' :: Prism' s a -> ((a -> s) -> (s -> Maybe a) -> r) -> r
+withPrism' p cont = withPrism p $ \build match ->
+    let match' s = case match s of
+          Right x -> Just x
+          Left _ -> Nothing
+    in cont build match'
+
+andThen :: Optic a x -> Optic Text y -> Optic a (x, y)
+andThen first second = withPrism' first $ \b m ->
+  withPrism' second $ \b' m' ->
+  let
+    match (a, ctx) = case m (a, ctx) of
+      Just (x, (Context unconsumed)) -> case m' (unconsumed, (Context "")) of
         Just (y, unconsumed') -> Just ((x,y), unconsumed')
         Nothing -> Nothing
       Nothing -> Nothing
 
-    build ctx (x,y) = case b' ctx y of
-      (t, Context ctx') -> b (Context (t <> ctx')) x
+    build ((x,y), ctx) = case b' (y, ctx) of
+       (t, Context ctx') -> b (x, (Context (t <> ctx')))
+   in prism' build match
 
-parseInContext :: Parsec T.Text () a -> T.Text -> Context -> Maybe (a, Context)
-parseInContext p input (Context after) = eitherToMaybe $
+parseInContext :: Parser a -> (Text, Context) -> Maybe (a, Context)
+parseInContext p (input, (Context after)) = eitherToMaybe $
   parse (liftA2 (\parsed unconsumed ->
     (parsed, Context (unconsumed <> after)))
     p getInput) "" input
 
-i :: Optic T.Text Italic
-i = Optic {
-    matchOptic = parseInContext italic
-  , buildOptic = \(Context after) (Italic txt) -> ("*" <> txt <> "*", Context after)
-}
-
-asPrism :: Optic a b -> Prism' (a, Context) (b, Context)
-asPrism (Optic match build) = prism' build' match'
+i :: Optic Text Italic
+i = prism' build match
   where
-    build' (b, ctx) = build ctx b
-    match' (a, ctx) = match a ctx
+    match = parseInContext italic
+    build (Italic txt, Context after) = ("*" <> txt <> "*", Context after)
 
 eitherToMaybe e = case e of
         Left _ -> Nothing
