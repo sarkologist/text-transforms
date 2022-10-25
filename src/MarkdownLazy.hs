00b493db5f9474c11e15ea8b9030b69a309767d2
Oon Guo Liang <oon.guo.liang@gmail.com>
2022-10-25 15:27:41 +0800

    problem: failing produces traversals given prisms

--- src/MarkdownLazy.hs
+++ src/MarkdownLazy.hs
@@ -1,19 +1,22 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 import MarkdownParse (withinMany)
 
 import Data.Text as T
 
-import Text.Parsec
-import Control.Lens (alongside, Prism', prism, prism', withPrism)
+import Text.Parsec hiding (many)
+import Control.Lens (Iso', Choice, Optic', alongside, Traversal', Prism', prism, prism', withPrism, failing, iso, swapped, mapping)
 import Control.Lens.TH
-import Control.Applicative hiding (many)
+import Control.Applicative hiding (many, some)
 
 type Parser a = Parsec Text () a
 newtype Context = Context Text deriving Show
-type Optic a b = Prism' (a, Context) (b, Context)
+type P p f a b = Optic' p f (a, Context) (b, Context)
+type Pprism a b = forall p f. (Choice p, Applicative f) => P p f a b
+type Ptraversal a b = forall f. Applicative f => P (->) f a b
 
 withPrism' :: Prism' s a -> ((a -> s) -> (s -> Maybe a) -> r) -> r
 withPrism' p cont = withPrism p $ \build match ->
@@ -21,10 +24,24 @@ withPrism' p cont = withPrism p $ \build match ->
           Right x -> Just x
           Left _ -> Nothing
     in cont build match'
-
 inContext p = alongside p id
 
-andThen :: Optic a x -> Optic Text y -> Optic a (x, y)
+_cons :: Iso' (x,[x]) [x]
+_cons = iso (\(x,y) -> x:y) (\(x:y) -> (x,y))
+
+none :: Pprism Text [a]
+none = prism' (\([], ctx) -> ("", ctx)) (\(_, ctx) -> Just ([], ctx))
+
+many :: Pprism Text x -> Pprism Text [x]
+many p = many' p
+
+some :: Pprism Text a -> Pprism Text [a]
+some p = andThen p (many' p) . swapped . mapping _cons . swapped
+
+many' :: Pprism Text a -> Pprism Text [a]
+many' p = failing (some p) none
+
+andThen :: Pprism a x -> Pprism Text y -> Pprism a (x, y)
 andThen first second = withPrism' first $ \b m ->
   withPrism' second $ \b' m' ->
   let
@@ -50,13 +67,13 @@ data Header = Header {
   _content :: Text
 } deriving Show
 
-i :: Optic Text Italic
+i :: Pprism Text Italic
 i = prism' build match
   where
     match = parseInContext $ Italic . pack <$> withinMany (char '*') (noneOf (['*']))
     build (Italic txt, Context after) = ("*" <> txt <> "*", Context after)
 
-h :: Int -> Optic Text Header
+h :: Int ->  Pprism Text Header
 h n = prism' build match
   where
     match = parseInContext $ Header n . pack <$> between (string (hashes n) *> char ' ') endOfLine (many1 (noneOf ['\n']))
