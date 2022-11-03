f18ecc3b4e7ebe18a7adc5cab6ffcd959e5bfd1e
Oon Guo Liang <oon.guo.liang@gmail.com>
2022-11-03 14:45:25 +0800

    _ easier to see than x

--- tests/MarkdownLazyTest.hs
+++ tests/MarkdownLazyTest.hs
@@ -12,24 +12,24 @@ spec_markdown_lazy :: Spec
 spec_markdown_lazy = do
  describe "markdown lazy" $ do
    it "italic" $
-     flip set "x" (text . i . _1 . unItalic)
+     flip set "_" (text . i . _1 . unItalic)
        "*i*" `shouldBe`
-       "*x*"
+       "*_*"
 
    it "many" $ do
-     flip set "x" (text . many' i . _1 . unItalic)
+     flip set "_" (text . many' i . _1 . unItalic)
        "*i**i2*" `shouldBe`
-       "*x**x*"
+       "*_**_*"
 
    it "indexing into many" $ do
-     flip set "x" (text . partsOf (many' i . _1) . ix 1 . unItalic)
+     flip set "_" (text . partsOf (many' i . _1) . ix 1 . unItalic)
        "*i**i2**i3*" `shouldBe`
-       "*i**x**i3*"
+       "*i**_**i3*"
 
    it "focusing" $ do
-     flip set "x" (text . h 1 . focusing content . i . _1 . unItalic)
+     flip set "_" (text . h 1 . focusing content . i . _1 . unItalic)
        "# *i* not i\n not h" `shouldBe`
-       "# *x* not i\n not h"
+       "# *_* not i\n not h"
 
    it "focusing after ||>" $ do
      flip set "_" (text . (h 1 ||> i) . focusing (_Left . content) . i . _1 . unItalic)
@@ -37,22 +37,22 @@ spec_markdown_lazy = do
        "# *_* not i\n*i outside*"
 
    it "focusing after many" $ do
-     flip set "x" (text . many' (h 1) . focusing content . i . _1 . unItalic)
+     flip set "_" (text . many' (h 1) . focusing content . i . _1 . unItalic)
        "# *i* not i\n# *i2* not i2\n# no i\n *i* not i" `shouldBe`
-       "# *x* not i\n# *x* not i2\n# no i\n *i* not i"
+       "# *_* not i\n# *_* not i2\n# no i\n *i* not i"
 
    it "fail <||> succeed" $ do
-     flip set "x" (text . (h 1 <||> i) . _1 . _Right . unItalic)
-       "*i*# h1\n" `shouldBe` "*x*# h1\n"
+     flip set "_" (text . (h 1 <||> i) . _1 . _Right . unItalic)
+       "*i*# h1\n" `shouldBe` "*_*# h1\n"
 
    it "succeed <||> fail" $ do
-     flip set "x" (text . (h 1 <||> i) . _1 . _Left . content)
-       "# h1\n*i*" `shouldBe` "# x\n*i*"
+     flip set "_" (text . (h 1 <||> i) . _1 . _Left . content)
+       "# h1\n*i*" `shouldBe` "# _\n*i*"
 
    it "<||> inside many: left succeeds" $ do
-     flip set "x" (text . many' (i <||> h 1) . _1 . _Left . unItalic)
-       "*i*# h1\n" `shouldBe` "*x*# h1\n"
+     flip set "_" (text . many' (i <||> h 1) . _1 . _Left . unItalic)
+       "*i*# h1\n" `shouldBe` "*_*# h1\n"
 
    it "<||> inside many: right succeeds" $ do
-     flip set "x" (text . many' (i <||> h 1) . _1 . _Right . content)
-       "*i*# h1\n" `shouldBe` "*i*# x\n"
+     flip set "_" (text . many' (i <||> h 1) . _1 . _Right . content)
+       "*i*# h1\n" `shouldBe` "*i*# _\n"
